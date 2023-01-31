2df32a74f20e4a72f69b3770d570098d09a76454
l3miage-bouissai <bouissailyass@gmail.com>
2023-01-31 16:33:49 +0100

    panier service update

--- src/app/services/panier/panier.service.ts
+++ src/app/services/panier/panier.service.ts
@@ -4,16 +4,23 @@ import { Injectable } from '@angular/core';
 import { lastValueFrom, Observable } from 'rxjs';
 import { ResultatAddProduit } from 'src/app/models/resultatAddProduit';
 import { PanierComponent } from 'src/app/pages/panier/panier.component';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PanierService {
 
+  private dataSubject = new Subject<Commande|undefined>();
+  panier$ = this.dataSubject.asObservable();
   panier : Commande | undefined;
 
   constructor(private httpClient: HttpClient) { }
 
+  updateData(panier: Commande | undefined) {
+    this.panier = panier;
+    this.dataSubject.next(this.panier);
+  }
 
   async addProduct(produit: number, quantiteCommande: number, idCompte: number): Promise<ResultatAddProduit> {
     const body = {
@@ -21,9 +28,7 @@ export class PanierService {
       "quantiteCommande": quantiteCommande,
       "idCompte": idCompte
     };
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
     try{
       let resultatAddProduit = await lastValueFrom(this.httpClient.post<ResultatAddProduit>('http://localhost:8080/presentation/resultat', body));
       return resultatAddProduit;
@@ -32,26 +37,28 @@ export class PanierService {
     }
   }
 
-/*  async getCommandeCourante(idCompte: number): Promise<Commande> {
-    let body = idCompte;
-    try{
-      let resultatAddProduit = await lastValueFrom(this.httpClient.post<Commande>('http://localhost:8080/commande/commandecourante', body, { headers: { 'Content-Type': 'application/json' }}));
-      return resultatAddProduit;
-    }catch(error: any){
-      throw new Error(error);
-    }
-  }*/
-
   getCommandeEnCours(commandelist:Commande[]) {
     let resultCommande : Commande | undefined = undefined;
     commandelist.forEach((commande) => {
       if(commande.status === 'en cours'){
         resultCommande = commande;
+        console.log("commande trouvée");
       }
     });
-    this.panier = resultCommande;
+    this.updateData(resultCommande);
+    localStorage.setItem('panier',JSON.stringify(resultCommande));
     console.log(this.panier);
   }
 
 
+  /*  async getCommandeCourante(idCompte: number): Promise<Commande> {
+      let body = idCompte;
+      try{
+        let resultatAddProduit = await lastValueFrom(this.httpClient.post<Commande>('http://localhost:8080/commande/commandecourante', body, { headers: { 'Content-Type': 'application/json' }}));
+        return resultatAddProduit;
+      }catch(error: any){
+        throw new Error(error);
+      }
+    }
+    */
 }
